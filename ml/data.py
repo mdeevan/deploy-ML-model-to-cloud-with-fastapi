419447d5539d16e05669b668e17e42beadc58750
Muhammad Naveed <mnaveed@outlook.com>
2025-03-21 16:28:39 +0000

    wip

--- ml/data.py
+++ ml/data.py
@@ -16,32 +16,32 @@ def process_data(
 
     Inputs
     ------
-    X : pd.DataFrame
-        Dataframe containing the features and label. Columns in `categorical_features`
-    categorical_features: list[str]
-        List containing the names of the categorical features (default=[])
-    label : str
-        Name of the label column in `X`. If None, then an empty array will be returned
-        for y (default=None)
-    training : bool
-        Indicator if training mode or inference/validation mode.
-    encoder : sklearn.preprocessing._encoders.OneHotEncoder
-        Trained sklearn OneHotEncoder, only used if training=False.
-    lb : sklearn.preprocessing._label.LabelBinarizer
-        Trained sklearn LabelBinarizer, only used if training=False.
+        X : pd.DataFrame
+            Dataframe containing the features and label. Columns in `categorical_features`
+        categorical_features: list[str]
+            List containing the names of the categorical features (default=[])
+        label : str
+            Name of the label column in `X`. If None, then an empty array will be returned
+            for y (default=None)
+        training : bool
+            Indicator if training mode or inference/validation mode.
+        encoder : sklearn.preprocessing._encoders.OneHotEncoder
+            Trained sklearn OneHotEncoder, only used if training=False.
+        lb : sklearn.preprocessing._label.LabelBinarizer
+            Trained sklearn LabelBinarizer, only used if training=False.
 
     Returns
     -------
-    X : np.array
-        Processed data.
-    y : np.array
-        Processed labels if labeled=True, otherwise empty np.array.
-    encoder : sklearn.preprocessing._encoders.OneHotEncoder
-        Trained OneHotEncoder if training is True, otherwise returns the encoder passed
-        in.
-    lb : sklearn.preprocessing._label.LabelBinarizer
-        Trained LabelBinarizer if training is True, otherwise returns the binarizer
-        passed in.
+        X : np.array
+            Processed data.
+        y : np.array
+            Processed labels if labeled=True, otherwise empty np.array.
+        encoder : sklearn.preprocessing._encoders.OneHotEncoder
+            Trained OneHotEncoder if training is True, otherwise returns the encoder passed
+            in.
+        lb : sklearn.preprocessing._label.LabelBinarizer
+            Trained LabelBinarizer if training is True, otherwise returns the binarizer
+            passed in.
     """
 
     if label is not None:
--- starter/train_model.py
+++ starter/train_model.py
@@ -1,10 +1,16 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+import pandas as pd
+from ml.data import process_data
 
 # Add the necessary imports for the starter code.
 
+
 # Add code to load in the data.
+data = pd.read_csv("../data/census_clean.csv")
+
+
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -24,5 +30,9 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test,  categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
+
 
 # Train and save a model.
